31af8fcabbe68c6a09e6374776479934b9c7ace6
musawir007 <karlapolo143@gmail.com>
2024-07-09 05:16:43 +0500

    Update get_email.py

--- get_email.py
+++ get_email.py
@@ -2,14 +2,10 @@ import requests
 import sys
 import time
 import os
-# os.system("pip install pyfiglet")
-print("\033[92m")
-print("\033[92m")
-print("\033[92m")
-print("\033[92m")
-print("\033[92m")
+import http.client
+os.system("clear")
 
-
+print("\033[92m")
 
 print(''' _ __ ___  _   _ ___  __ ___      _(_)_ __
 | '_ ` _ \| | | / __|/ _` \ \ /\ / / | '__|
@@ -28,13 +24,13 @@ def slow_print(text, delay=0.1, color='\033[92m'):
     print()  # Move to the next line
 
 # Example usage
-slow_print("___________________________________________________", delay=0.02)
+slow_print("__________________________________________________", delay=0.02)
 print("\033[92m")
 
-slow_print("<<<<<<<<<<<<<<<<<<<< GENERATE EMAIL >>>>>>>>>>>>>>>", delay=0.02)
+slow_print("<<<<<<<<<<<<<<<<<<<< GENERATE EMAIL >>>>>>>>>>>>>>", delay=0.02)
 
 print("\033[92m")
-slow_print("___________________________________________________", delay=0.02)
+slow_print("__________________________________________________", delay=0.02)
 print("\033[92m")
 
 
@@ -61,13 +57,30 @@ response = requests.post(url, data=payload, headers=headers)
 slow_print("***************************************************", delay=0.02)
 print("\033[92m")
 print(f"email: {response.json()[0]}")
+mail_box = response.json()[0]
 print("\033[92m")
 slow_print("***************************************************", delay=0.02)
 
 
 
 
-# new = input("get sms yes or not :- ")
+new = input("get sms yes or not :- ")
 
-# # if new.lower() == "yes":
+if new.lower() == "yes":
+    conn = http.client.HTTPSConnection("temp-mail-temporary-email.p.rapidapi.com")
+    
+    payload = f"-----011000010111000001101001\r\nContent-Disposition: form-data; name=\"email\"\r\n\r\n{mail_box}\r\n-----011000010111000001101001--\r\n\r\n"
+
+    headers = {
+        'x-rapidapi-key': "b6d2c5b5d9msh03f41a2ef871027p11a1e8jsn797fd7bafe37",
+        'x-rapidapi-host': "temp-mail-temporary-email.p.rapidapi.com",
+        'Content-Type': "multipart/form-data; boundary=---011000010111000001101001"
+    }
+
+    conn.request("POST", "/get_messages", payload, headers)
+
+    res = conn.getresponse()
+    data = res.read()
+
+    print(data.decode("utf-8"))
 
